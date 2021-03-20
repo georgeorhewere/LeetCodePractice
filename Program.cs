f8a93412cb43144c6aedfd7c8bcd38cb4ccf4131
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-03-20 10:54:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCodePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leet Code Exercises!");
+            testShortestSubArray();
+            Console.ReadKey();
+        }
+
+        static  void testShortestSubArray()
+        {
+            ShortestSubArray manager = new ShortestSubArray();
+            int[] testArray = new int[] { 1, 2, 3, 10, 4, 2, 3, 5 };
+            int result = manager.FindLengthOfShortestSubarray(testArray);
+            Console.WriteLine($"The Length of the shortest subarray is {result}");
+
+        }
+    }
+}
---
+++ ShortestSubArray/ShortestSubArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    public class ShortestSubArray
+    {
+
+        public int FindLengthOfShortestSubarray(int[] arr)
+        {
+
+            return 0;
+        }
+    }
+}
